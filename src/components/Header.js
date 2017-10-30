4ce1d1d5c10868402794d11e34fcf6fdaab99c40
nick-occ <nicholaso1980@gmail.com>
2017-10-30 16:00:14 -0400

    Removed edit link from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
     <header>
     <h1>Expensify</h1>
-    <NavLink to='/' activeClassName="is-active" exact={true} >Home</NavLink>
-    <NavLink to='/create' activeClassName="is-active">Create</NavLink>
-    <NavLink to='/edit' activeClassName="is-active">Edit</NavLink>
+    <NavLink to='/' activeClassName="is-active" exact={true} >Dashboard</NavLink>
+    <NavLink to='/create' activeClassName="is-active">Create Expense</NavLink>
     </header>
 );
 
