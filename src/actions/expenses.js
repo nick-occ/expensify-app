c48dbcb77b132bfbb032d0ccd5cedf83676e0d80
nick-occ <nicholaso1980@gmail.com>
2017-11-02 17:28:11 -0400

    Added functionality to set expenses from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -39,4 +39,28 @@ export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
-})
+});
+
+// SET_EXPENSES
+export const setExpenses = (expenses) =>({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = ()=>{
+    return (dispatch)=>{
+        return database.ref('expenses').once('value')
+        .then((snapshot)=>{
+            
+            const expenses = [];
+            snapshot.forEach((expense)=>{
+                expenses.push({
+                    id: expense.key,
+                    ...expense.val()
+                });
+            });
+            console.log(expenses);
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
